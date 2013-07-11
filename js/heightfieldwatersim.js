d983798972245d82b147d99d935d34f062caee09
Skeel <skeel@skeelogy.com>
2013-07-12 05:21:53 +0800

    Implemented Muller GDC2008 height field water sim

--- js/heightfieldwatersim.js
+++ js/heightfieldwatersim.js
@@ -3,16 +3,21 @@
 //@since: 11 July 2013
 //2D height-field water simulation
 
-function HeightFieldWaterSim(mesh, size, res)
+function HeightFieldWaterSim(mesh, size, res, dampingFactor)
 {
 	this.mesh = mesh;
 	this.size = size;
 	this.res = res;
-
-	this.dampingFactor = 0.99;
+	this.dampingFactor = dampingFactor;
 
 	this.geometry = this.mesh.geometry;
 	this.numVertices = res * res;
+	if (this.numVertices !== this.geometry.vertices.length)
+	{
+		throw new Error('Number of vertices in mesh does not match res*res');
+	}
+	this.segmentSize = this.size / this.res;
+	this.segmentSizeSquared = this.segmentSize * this.segmentSize;
 
 	this.velocityField = [];
 	this.sourceField = [];
@@ -23,21 +28,13 @@ function HeightFieldWaterSim(mesh, size, res)
 
 HeightFieldWaterSim.prototype.init = function()
 {
-	//init the arrays
+	//init fields first
 	var i;
 	for (i = 0; i < this.numVertices; i++)
 	{
 		this.velocityField[i] = 0;
 		this.sourceField[i] = 0;
 		this.obstacleField[i] = 0;
-	}
-
-	//init with some interesting shape
-	var v = this.mesh.geometry.vertices;
-	var len = v.length;
-	for (i = 0; i < len; i++)
-	{
-		v[i].y = Math.sin(v[i].x);
 	}
 
 	//update mesh
@@ -47,31 +44,27 @@ HeightFieldWaterSim.prototype.init = function()
 HeightFieldWaterSim.prototype.update = function(dt)
 {
 	this.sim(dt);
+	this.__clearFields();
 }
 
 HeightFieldWaterSim.prototype.sim = function(dt)
 {
-	var i, j, idx;
-	var v = this.mesh.geometry.vertices;
-	for (i = 1; i < this.res-1; i++)
-	{
-		for (j = 1; j < this.res-1; j++)
-		{
-			idx = i * this.res + j;
-			this.velocityField[idx] += (v[(i-1)*this.res+j].y + v[(i+1)*this.res+j].y + v[i*this.res+(j-1)].y + v[i*this.res+(j+1)].y) / 4.0 - v[idx].y;
-			this.velocityField[idx] *= this.dampingFactor;
-		}
-	}
+	throw new Error('Abstract method not implemented');
+}
 
-	//update vertex heights
-	var len = v.length;
-	for (i = 0; i < len; i++)
-	{
-		v[i].y += this.velocityField[i];
-	}
+HeightFieldWaterSim.prototype.disturb = function(idx, amount)
+{
+	this.sourceField[idx] += amount;
+}
 
-	//update mesh
-	this.__updateMesh();
+HeightFieldWaterSim.prototype.__clearFields = function()
+{
+	var i;
+	for (i = 0; i < this.numVertices; i++)
+	{
+		this.sourceField[i] = 0;
+		this.obstacleField[i] = 0;
+	}
 }
 
 HeightFieldWaterSim.prototype.__updateMesh = function()
@@ -80,4 +73,62 @@ HeightFieldWaterSim.prototype.__updateMesh = function()
 	this.geometry.computeFaceNormals();  //must call this first before computeVertexNormals()
 	this.geometry.computeVertexNormals();
 	this.geometry.normalsNeedUpdate = true;
+}
+
+
+
+function HeightFieldWaterSim_Muller_GDC2008(mesh, size, res, dampingFactor, horizontalSpeed)
+{
+	HeightFieldWaterSim.call(this, mesh, size, res, dampingFactor);
+
+	this.horizontalSpeed = horizontalSpeed;
+	this.horizontalSpeedSquared = this.horizontalSpeed * this.horizontalSpeed;
+}
+//inherit from HeightFieldWaterSim
+HeightFieldWaterSim_Muller_GDC2008.prototype = Object.create(HeightFieldWaterSim.prototype);
+HeightFieldWaterSim_Muller_GDC2008.prototype.constructor = HeightFieldWaterSim_Muller_GDC2008;
+//override
+HeightFieldWaterSim_Muller_GDC2008.prototype.sim = function(dt)
+{
+	var i, j, idx;
+	var v = this.geometry.vertices;
+
+	//add source field first
+	for (i = 1; i < this.res-1; i++)
+	{
+		for (j = 1; j < this.res-1; j++)
+		{
+			idx = i * this.res + j;
+			v[idx].y += this.sourceField[idx];
+		}
+	}
+
+	//calculate vertical acceleration and velocity
+	var acc;
+	for (i = 1; i < this.res-1; i++)
+	{
+		for (j = 1; j < this.res-1; j++)
+		{
+			idx = i * this.res + j;
+			acc = this.horizontalSpeedSquared * (
+				v[idx+this.res].y    //height[i+1,j]
+				+ v[idx-this.res].y  //height[i-1,j]
+				+ v[idx+1].y         //height[i,j+1]
+				+ v[idx-1].y         //height[i,j-1]
+				- 4 * v[idx].y       //4 * height[i,j]
+				) / this.segmentSizeSquared;
+			this.velocityField[idx] += acc * dt;
+			this.velocityField[idx] *= this.dampingFactor;
+		}
+	}
+
+	//update vertex heights
+	var len = v.length;
+	for (i = 0; i < len; i++)
+	{
+		v[i].y += this.velocityField[i] * dt;
+	}
+
+	//update mesh
+	this.__updateMesh();
 }
