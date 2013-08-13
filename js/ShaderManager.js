7956fb0b6fafb50f08342b9121b543bffa4a6199
Skeel <skeel@skeelogy.com>
2013-08-13 14:54:17 +0800

    JSLint checks on ShaderManager.js

--- js/ShaderManager.js
+++ js/ShaderManager.js
@@ -8,27 +8,31 @@ THREE.ShaderManager = {
 
     shaderContents: {},
 
-    addShader: function(url, async) {
-        var async = typeof async === 'undefined' ? false : async;
-        if (typeof async !== 'boolean') throw new Error('parameter "async" must be a boolean');
-        this.__loadShaderContents(url, async)
+    addShader: function (url, async) {
+        async = typeof async === 'undefined' ? false : async;
+        if (typeof async !== 'boolean') {
+            throw new Error('parameter "async" must be a boolean');
+        }
+        this.__loadShaderContents(url, async);
     },
 
-    __loadShaderContents: function(url, async) {
+    __loadShaderContents: function (url, async) {
         var that = this;
         $.ajax({
             url: url,
             async: async
-        }).done(function(data){
+        }).done(function (data) {
             that.shaderContents[url] = data;
-        }).error(function(xhr, textStatus, error){
+        }).error(function (xhr, textStatus, error) {
             throw new Error('error loading ' + url + ': ' + error);
         });
     },
 
-    getShaderContents: function(url) {
+    getShaderContents: function (url) {
         var content = this.shaderContents[url];
-        if (!content) throw new Error('Unable to access shader content using key: ' + url);
+        if (!content) {
+            throw new Error('Unable to access shader content using key: ' + url);
+        }
         return content;
     }
 };
