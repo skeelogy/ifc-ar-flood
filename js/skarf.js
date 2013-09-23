67b09241863315e80894924a17aec06ce13cc4ac
Skeel <skeel@skeelogy.com>
2013-09-23 23:33:44 +0800

    Included license info

--- js/skarf.js
+++ js/skarf.js
@@ -1,4 +1,24 @@
 /**
+ * skarf.js
+ * A JavaScript augmented reality framework for handling augmented reality libraries
+ * 
+ * Copyright (C) 2013 Skeel Lee (skeel@skeelogy.com)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see [http://www.gnu.org/licenses/].
+ */
+
+/**
  * @fileOverview A JavaScript augmented reality framework for handling augmented reality libraries
  * @author Skeel Lee <skeel@skeelogy.com>
  * @version 1.0.0
